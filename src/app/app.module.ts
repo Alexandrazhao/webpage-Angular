51ee0ef203867a53aa3389002e0701dbe4de8425
Alexandrazhao <yz3514@bard.edu>
2021-02-18 14:28:17 -0800

    Angular reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,14 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { FormsModule } from '@angular/forms'; 
+import { ReactiveFormsModule } from '@angular/forms'; 
+
 //import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 
 import 'hammerjs';
 import { MenuComponent } from './menu/menu.component';
@@ -49,6 +54,7 @@ import { LoginComponent } from './login/login.component';
     BrowserAnimationsModule, 
     FlexLayoutModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     MatToolbarModule,
     MatListModule,
@@ -58,7 +64,9 @@ import { LoginComponent } from './login/login.component';
     MatDialogModule,
     MatFormFieldModule,
     MatInputModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatSelectModule,
+    MatSlideToggleModule
   ],
   providers: [
     DishService,
