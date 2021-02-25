89f4efcbee3af968f6f4f6b0b6681963d1fca131
Alexandrazhao <yz3514@bard.edu>
2021-02-24 22:02:22 -0800

    Http Part 2, error handling

--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -4,26 +4,30 @@ import { Observable, of } from 'rxjs';
 import { delay } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { baseURL } from '../shared/baseurl';
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
+import { ProcessHTTPMsgService } from './process-httpmsg.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DishService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private processHttpMsgService: ProcessHTTPMsgService) { }
 
   getDishes(): Observable<Dish[]>{
-    return this.http.get<Dish[]>(baseURL + 'dishes');
+    return this.http.get<Dish[]>(baseURL + 'dishes')
+      .pipe(catchError(this.processHttpMsgService.handleError));
   }
   getDish(id: string): Observable<Dish> {
     return this.http.get<Dish>(baseURL + 'dishes/' + id);
   }
   getFeaturedDish(): Observable<Dish> {
     return this.http.get<Dish[]>(baseURL + 'dishes?featured=true')
-      .pipe(map(dishes => dishes[0]));
+      .pipe(map(dishes => dishes[0]))
+      .pipe(catchError(this.processHttpMsgService.handleError));
   }
   getDishIds():Observable<String[] | any>{
-    return this.getDishes().pipe(map(dishes => dishes.map(dish => dish.id)));
+    return this.getDishes().pipe(map(dishes => dishes.map(dish => dish.id)))
+    .pipe(catchError(error=> error));
   }
 }
